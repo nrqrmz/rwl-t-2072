bf8981a2e1d0c0bb53bbaf31242397db604fa7a2
Enrique Ramirez <nrqrmz@gmail.com>
2025-09-18 12:05:57 -0600

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'uri'
+require 'net/http'
+require 'json'
+
+url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=2")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDU5NmY1ZDIyOTg4MjU3OTIyNmMzYzAzZTNlMjZkNiIsIm5iZiI6MTc0ODEwMDQ3NS45NTM5OTk4LCJzdWIiOiI2ODMxZTU3YjZkNzA3OGM4NjE0MTM5N2UiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.iRuH55Wvv30SIoozib-XT2F43cWMXo5xD6LcelV02t8'
+
+response = JSON.parse(http.request(request).read_body)
+
+results = response['results']
+
+results.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
